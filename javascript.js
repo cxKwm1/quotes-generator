b45f826727f993aa4476f85e7d30c9ce8436b05a
kylematsui <63168744+cxKwm1@users.noreply.github.com>
2020-08-27 17:29:03 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
+var quotes = [
+  "test"
+  "test2"
+  "test3"
+  "test4"
+]
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random) * (quotes.length));
+  document.getElementId('quoteDisplay').innerHTML = quotes[randomNumber];
+}
 
